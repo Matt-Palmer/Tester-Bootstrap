dd8a9b5e68ffbc62a480398c532ae1371ba65625
matthewp <matthew.palmer@signify.net>
2016-04-29 10:32:27 +0100

    added a carousel

--- js/javascript.js
+++ js/javascript.js
@@ -15,7 +15,7 @@ $(window).scroll(function(){
         for (var i=0; i < aArray.length; i++) {
             var theID = aArray[i];
             var divPos = $(theID).offset().top; // get the offset of the div from the top of page
-            var divHeight = $(theID).height(); // get the height of the div in question
+            var divHeight = $(theID).height();; // get the height of the div in question
             if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
                 $("a[href='" + theID + "']").addClass("nav-active");
             } else {
